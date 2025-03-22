9de1cab5cf974c823b0b750a60bc5131d070fe54
Elmane091203 <djaanffarhoumadielmane@gmail.com>
2025-03-22 15:50:25 +0000

    integration du template

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/', name: 'app_login')]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+    #[Route('/logon', name:'app_logon')]
+    public function logon(): Response
+    {
+        return $this->redirectToRoute('app_home');
+    }
+}
